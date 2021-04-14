fa3d72c182b4ccf447b02c10d2ebc29b317398d5
vaibhya <vk751999@gmail.com>
2021-04-14 19:45:29 +0530

    2nd commit

---
+++ src/com/headFirst/Chapter2/DrumKit.java
@@ -0,0 +1,12 @@
+package com.headFirst.Chapter2;
+
+public class DrumKit {
+	boolean topHat = true;
+	boolean snare = true;
+	public void playTopHat() {
+		 System.out.println("ding ding da-ding");
+	}
+	public void playSnare() {
+		System.out.println("bang bang ba-bang");
+	}
+}
---
+++ src/com/headFirst/Chapter2/DrumKitTest.java
@@ -0,0 +1,14 @@
+package com.headFirst.Chapter2;
+public class DrumKitTest {
+	public static void main(String args[]) {
+		DrumKit d = new DrumKit();
+		d.playTopHat();
+		d.playSnare();
+		d.snare = false;
+		if (d.snare = true) {
+			d.playSnare();
+		}
+		
+	}
+	
+}
